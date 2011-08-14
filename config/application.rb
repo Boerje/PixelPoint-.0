f6f525904b4a5e5deb4b873b0bae15763178ba15
Boerje <boerje.santen@freenet.de>
2011-08-14 17:52:53 +0200

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+
 module Pixelpoint
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -36,7 +45,7 @@ module Pixelpoint
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
+   # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
 end
